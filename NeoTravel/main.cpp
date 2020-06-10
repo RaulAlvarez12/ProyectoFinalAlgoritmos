d895d3fe0bb7d9d6091a300bcfb5efb1b7239e4b
RaulAlvarez12 <raulalvf@gmail.com>
2020-06-09 21:02:50 -0600

    Prueba con un cout

--- NeoTravel/main.cpp
+++ NeoTravel/main.cpp
@@ -1,11 +1,11 @@
 /* 
  * File:   main.cpp
  * Author: Raul Alvarez Fernandez
- *
  * Created on 9 de junio de 2020, 08:44 PM
  */
 
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
@@ -13,6 +13,7 @@ using namespace std;
  * 
  */
 int main(int argc, char** argv) {
-
+    
+    cout << "Prueba git hub" << endl;
     return 0;
-}
+}//finMain
