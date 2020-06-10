5439f7b1bc26dca1301eea32202fa3f716ba7345
AlsovCR <alem08@outlook.es>
2020-06-09 21:56:52 -0600

    Agrega clase Triangulo

---
+++ NeoTravel/Triangulo.h
@@ -0,0 +1,22 @@
+
+/* 
+ * File:   Triangulo.h
+ * Author: alsov
+ *
+ * Created on 9 de junio de 2020, 09:55 PM
+ */
+
+#ifndef TRIANGULO_H
+#define TRIANGULO_H
+
+class Triangulo {
+public:
+    Triangulo();
+    Triangulo(const Triangulo& orig);
+    virtual ~Triangulo();
+private:
+
+};
+
+#endif /* TRIANGULO_H */
+
