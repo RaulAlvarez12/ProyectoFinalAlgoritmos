8f7a9870015be2e827703b3c3d06394cc401abb1
RaulAlvarez12 <raulalvf@gmail.com>
2020-06-09 20:51:38 -0600

    Carga del master vacio

---
+++ NeoTravel/main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ * Author: Raul Alvarez Fernandez
+ *
+ * Created on 9 de junio de 2020, 08:44 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
